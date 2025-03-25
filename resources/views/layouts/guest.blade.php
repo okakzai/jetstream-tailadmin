c96a03270e8c403be3eb81959b1ad42956b81509
unknown <okakzai@gmail.com>
2025-03-25 10:36:25 +0700

    Jetstream: Sign In

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,21 +7,35 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
-        @livewireStyles
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" href="{{ asset('template/tailadmin/style.css') }}">
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body
+        x-data="@yield('data', "{ page: 'comingSoon', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }")"
+        x-init="
+            darkMode = JSON.parse(localStorage.getItem('darkMode'));
+            $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+        :class="{'dark bg-gray-900': darkMode === true}"
+    >
+        <!-- ===== Preloader Start ===== -->
+        <div
+        x-show="loaded"
+        x-init="window.addEventListener('DOMContentLoaded', () => {setTimeout(() => loaded = false, 500)})"
+        class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black"
+        >
+            <div
+            class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-brand-500 border-t-transparent"
+            >
+            </div>
+        </div>
+        <!-- ===== Preloader End ===== -->
+        
+        <!-- ===== Page Wrapper Start ===== -->
+        <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
             {{ $slot }}
         </div>
+        <!-- ===== Page Wrapper End ===== -->
 
-        @livewireScripts
+        <!-- @livewireScripts -->
     </body>
 </html>
